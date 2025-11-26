042b4605331119881b028f9d2dabfbcd6417ff63
Sus.diaz <sus.diazse@gmail.com>
2025-11-26 08:57:12 +0100

    Add array to country list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,67 @@ import "../styles/App.scss";
 import Header from "./layout/Header";
 
 function App() {
+
+  const paises = [
+    {
+      commonName: "España",
+      officialName: "Reino de España",
+    },
+     {
+      commonName: "Italia",
+      officialName: "República Italiana",
+    },
+    {
+      commonName: "Marruecos",
+      officialName: "Reino de marruecos",
+    },
+     {
+      commonName: "Egipto",
+      officialName: "República arábica de Egipto",
+    },
+    {
+      commonName: "Estados Unicos",
+      officialName: "Estados Unidos de América",
+    },
+    {
+      commonName: "Canadá",
+      officialName: "Canadá",
+    },
+    {
+      commonName: "Argentina",
+      officialName: "República Argentina",
+    },
+    {
+      commonName: "México",
+      officialName: "Estados Unidos Mexicanos",
+    },
+     {
+      commonName: "China",
+      officialName: "República Popular de China",
+    },
+     {
+      commonName: "Japón",
+      officialName: "Japón",
+    },
+     {
+      commonName: "Japón",
+      officialName: "Japón",
+    },
+     {
+      commonName: "Australia",
+      officialName: "Australia",
+    },
+     {
+      commonName: "Nueva Zelanda",
+      officialName: "Nueva Zelanda",
+    },
+  ];
+
   return (
     <div>
       <Header />
-      <main className="main">Holis Adalabers!!!</main>
+      <main className="main">
+      </main>
     </div>
   );
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="header">
-      <h1 className="title">Plantilla React</h1>
+      <h1 className="title">Listado de países</h1>
     </header>
   );
 }
