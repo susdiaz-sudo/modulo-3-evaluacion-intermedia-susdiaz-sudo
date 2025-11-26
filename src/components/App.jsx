48e1c0111290f77a9e84fc3100c3cc3e92e32f21
Sus.diaz <sus.diazse@gmail.com>
2025-11-26 08:33:41 +0100

    Add starter kit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import "../styles/App.scss";
+import Header from "./layout/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="main">Holis Adalabers!!!</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="title">Plantilla React</h1>
+    </header>
+  );
+}
+
+export default Header;
